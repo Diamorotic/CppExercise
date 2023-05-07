70ce3f0ae6cd1dd4e0aeb99009ced0c8919560f4
Diamorotic <diamorotic@gmail.com>
2023-05-07 21:42:44 +0200

    CCI-Cpp-Ex2 - In-place reverting with std::swap (#2)

---
+++ CppExercise/CciCppEx2.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+
+// Reverse a null terminated string. 
+class CciCppEx2 {
+public:
+	static void execute(char *input);
+	static void printArr(char *input);
+};
+
+
