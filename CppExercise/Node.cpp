1dfb6ebb9455408386333efe298d04925e7c2917
Diamorotic <diamorotic@gmail.com>
2023-05-09 16:34:51 +0200

    DeepCopy implemented.

--- CppExercise/Node.cpp
+++ CppExercise/Node.cpp
@@ -1,1 +1,71 @@
 #include "Node.h"
+
+Node::Node(int newVal) : val(newVal) {}
+Node::Node(int newVal, Node *newLeft, Node *newRight) : val(newVal), left(newLeft), right(newRight) {}
+
+Node* Node::L() const {
+	return left;
+}
+Node* Node::R() const {
+	return right;
+}
+int Node::V() const {
+	return val;
+}
+bool Node::wasVisited() const {
+	return visited;
+}
+
+void Node::sL(Node *newLeft) {
+	left = newLeft;
+}
+void Node::sR(Node *newRight) {
+	right = newRight;
+}
+void Node::setV(int newVal) {
+	val = newVal;
+}
+void Node::visit() {
+	visited = true;
+}
+void Node::visit(std::unordered_set<Node*> &visitedNodes) {
+	visit();
+	visitedNodes.insert(this);
+}
+void Node::unvisit() {
+	visited = false;
+}
+void Node::unvisit(std::unordered_set<Node*> &visitedNodes) {
+	for(auto x : visitedNodes) {
+		x->unvisit();
+	}
+}
+void Node::unvisit(std::unordered_map<Node*, Node*> &visitedNodes) {
+	for(auto x : visitedNodes) {
+		x.first->unvisit();
+	}
+}
+
+Node* Node::deepCopy() {
+	std::unordered_map<Node*, Node*> orgAndNew;
+
+	Node *res = deepCopyHelper(orgAndNew, this);
+	unvisit(orgAndNew);
+	return res;
+}
+Node* Node::deepCopyHelper(std::unordered_map<Node*, Node*> &orgAndNew, Node *org) {
+	if(org == nullptr)
+		return nullptr;
+	
+	if(orgAndNew.count(org) != 0) 
+		return orgAndNew[org];
+	
+	org->visit();
+	Node *res = new Node(org->V());
+	orgAndNew[org] = res;
+	Node *newL = deepCopyHelper(orgAndNew, org->L());
+	Node *newR = deepCopyHelper(orgAndNew, org->R());
+	res->sL(newL);
+	res->sR(newR);
+	return res;
+}
--- CppExercise/Node.h
+++ CppExercise/Node.h
@@ -1,45 +1,31 @@
 #pragma once
-class Node {
-	Node *left, *right;
-	int val;
-	bool visited = false;
-public:
-	Node() : val(0), left(nullptr), right(nullptr) {}
-	Node(int newVal) : val(newVal), left(nullptr), right(nullptr) {}
-	Node(int newVal, Node *newLeft, Node *newRight) : val(newVal), left(newLeft), right(newRight) {}
-	Node *L() const {
-		return left;
-	}
-	Node *R() const {
-		return right;
-	}
-	int V() const {
-		return val;
-	}
-	bool visited() const {
-		return visited;
-	}
-	void sL(Node *newLeft) {
-		left = newLeft;
-	}
-	void sR(Node *newRight) {
-		right = newRight;
-	}
-	void setV(int newVal) {
-		val = newVal;
-	}
-	void visit() {
-		visited = true;
-	}
-	Node *deepCopy() {
-		Node *newL, *newR, *res;
-		visit();
-		if(left != nullptr) {
-			if(left->visited) {
-				newL = 
-			}
-		}
-        //return new Node(val, left != nullptr ? left->deepCopy() : nullptr, right != nullptr ? right->deepCopy() : nullptr);
+#include <unordered_map>
+#include <unordered_set>
 
-	}
+class Node {
+	Node *left = nullptr;
+	Node *right = nullptr;
+	int val = 0;
+	bool visited = false;
+
+public:
+	Node(int newVal);
+	Node(int newVal, Node *newLeft, Node *newRight);
+
+	Node* L() const;
+	Node* R() const;
+	int V() const;
+	bool wasVisited() const;
+
+	void sL(Node *newLeft);
+	void sR(Node *newRight);
+	void setV(int newVal);
+	void visit();
+	void visit(std::unordered_set<Node*> &visitedNodes);
+	void unvisit();
+	void unvisit(std::unordered_set<Node*> &visitedNodes);
+	void unvisit(std::unordered_map<Node*, Node*> &visitedNodes);
+
+	Node* deepCopy();
+	static Node* deepCopyHelper(std::unordered_map<Node*, Node*> &orgAndNew, Node *org);
 };
